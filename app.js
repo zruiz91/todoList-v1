e1d7567fd8b19a22c0955cbdd9e315685ee900fd
Zach Ruiz <powersbooth10@gmail.com>
2023-05-12 13:14:49 -0500

    added a switch statement to find and display what day of the week it is.

--- app.js
+++ app.js
@@ -17,11 +17,48 @@ app.get('/', function (req, res) {
 
     let day = '';
 
-    if (currentDay === 6 || currentDay === 0) {
-        day = 'weekend'
-    } else {
-        day = 'weekday'
-    }
+    //switch statement to find the day of the week and assinging it to the day variable
+    switch (currentDay) {
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Tuesday";
+            break;
+        case 3:
+            day = "Wednesday";
+            break;
+        case 4:
+            day = "Thursday";
+            break;
+        case 5:
+            day = "Friday";
+            break;
+        case 6:
+            day = "Saturday";
+            break;
+        case 7:
+            day = "Sunday";
+            break;
+        default:
+            console.log("Error: current day is equal to " + currentDay);
+    };
+
+    // if (currentDay === 1) {
+    //     day = 'monday'
+    // } else if (currentDay === 2) {
+    //     day = 'tuesday'
+    // } else if (currentDay === 3) {
+    //     day = 'wednesday'
+    // } else if (currentDay === 4) {
+    //     day = 'thursday'
+    // } else if (currentDay === 5) {
+    //     day = 'friday'
+    // } else if (currentDay === 6) {
+    //     day = 'saturday'
+    // } else() {
+    //     day = 'sunday'
+    // }
 
     res.render("list", { kindOfDay: day });
 });
