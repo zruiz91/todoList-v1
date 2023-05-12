faa77e3a3502fe4982a845d78c10149ab270e500
Zach Ruiz <powersbooth10@gmail.com>
2023-05-12 13:39:56 -0500

    used the javascript toLocaleDateString to produce todays date formatted on the page.

--- app.js
+++ app.js
@@ -13,54 +13,47 @@ app.get('/', function (req, res) {
 
     let today = new Date();
 
-    let currentDay = today.getDay();
+    // let currentDay = today.getDay();
 
-    let day = '';
-
-    //switch statement to find the day of the week and assinging it to the day variable
-    switch (currentDay) {
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Tuesday";
-            break;
-        case 3:
-            day = "Wednesday";
-            break;
-        case 4:
-            day = "Thursday";
-            break;
-        case 5:
-            day = "Friday";
-            break;
-        case 6:
-            day = "Saturday";
-            break;
-        case 7:
-            day = "Sunday";
-            break;
-        default:
-            console.log("Error: current day is equal to " + currentDay);
+    let options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long',
     };
 
-    // if (currentDay === 1) {
-    //     day = 'monday'
-    // } else if (currentDay === 2) {
-    //     day = 'tuesday'
-    // } else if (currentDay === 3) {
-    //     day = 'wednesday'
-    // } else if (currentDay === 4) {
-    //     day = 'thursday'
-    // } else if (currentDay === 5) {
-    //     day = 'friday'
-    // } else if (currentDay === 6) {
-    //     day = 'saturday'
-    // } else() {
-    //     day = 'sunday'
-    // }
 
-    res.render("list", { kindOfDay: day });
+    let day = today.toLocaleDateString('en-US', options);
+
+    // //switch statement to find the day of the week and assinging it to the day variable
+    // switch (currentDay) {
+    //     case 1:
+    //         day = "Monday";
+    //         break;
+    //     case 2:
+    //         day = "Tuesday";
+    //         break;
+    //     case 3:
+    //         day = "Wednesday";
+    //         break;
+    //     case 4:
+    //         day = "Thursday";
+    //         break;
+    //     case 5:
+    //         day = "Friday";
+    //         break;
+    //     case 6:
+    //         day = "Saturday";
+    //         break;
+    //     case 7:
+    //         day = "Sunday";
+    //         break;
+    //     default:
+    //         console.log("Error: current day is equal to " + currentDay);
+    // };
+
+    res.render("list", {
+        kindOfDay: day
+    });
 });
 
 app.listen(process.env.PORT || 3000, function () {
