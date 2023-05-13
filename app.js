46ab4b056f76c32bbed9622dc6cf54a30f1f1712
Zach Ruiz <powersbooth10@gmail.com>
2023-05-13 11:45:34 -0500

    added stylesheet to the site using express.static to access the public folder.

--- app.js
+++ app.js
@@ -10,7 +10,10 @@ let items = ["buy food", "cook food", "eat food"];
 
 app.set('view engine', 'ejs');
 
+
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
 
 app.get('/', function (req, res) {
     // res.sendFile(__dirname + '/index.html');
